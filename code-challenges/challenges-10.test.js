4fd10ab07dcbca4df1b54016e31d1fd1e619763a
Matthew Holder <holdermatthew5@gmail.com>
2020-10-25 22:58:56 -0700

    challenge 02 done

--- code-challenges/challenges-10.test.js
+++ code-challenges/challenges-10.test.js
@@ -33,7 +33,15 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  let count = 0;
+  input.map(arr => {
+    let accumulate = arr.reduce((accumulator, value, index) => {
+      if (value === target) {accumulator++};
+      return accumulator;
+    }, 0);
+    count += accumulate;
+  })
+  return count;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -153,7 +161,7 @@ Run your tests from the console: jest challenges-10.test.js
 
 ------------------------------------------------------------------------------------------------ */
 
-describe('Testing challenge 1', () => {
+xdescribe('Testing challenge 1', () => {
 
   const request = require('supertest');
 
@@ -192,7 +200,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return the number of times the input is in the nested arrays', () => {
     expect(count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(4);
     expect(count(3, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(2);
