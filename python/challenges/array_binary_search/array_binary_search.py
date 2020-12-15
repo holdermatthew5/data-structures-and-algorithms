dfa1621753a60badac67e168b292acbcfa2278d1
Matthew Holder <holdermatthew5@gmail.com>
2020-12-14 20:45:14 -0800

    README.md

--- python/challenges/array_binary_search/array_binary_search.py
+++ python/challenges/array_binary_search/array_binary_search.py
@@ -1,6 +1,22 @@
 def binary_search(li, key):
+    '''
+        This function takes in an ordered list and an integer. It returns the index of the integer. If the integer is not in the list it returns -1.
+    '''
     i = 0
     while i < len(li):
         if li[i] == key:
             return i
     return -1
+
+# Tests
+def test_search():
+    assert binary_search([1,2,3,4,5,6,7,8], 3) == 2
+
+def test_search():
+    assert binary_search([0,1,2,3,4,5,6,7,8], 9) == -1
+
+def test_search_three():
+    assert binary_search([54,66,73,82,99], 82) == 3
+
+def test_search_two():
+    assert binary_search([54,66,73,82,99], 72) == -1
