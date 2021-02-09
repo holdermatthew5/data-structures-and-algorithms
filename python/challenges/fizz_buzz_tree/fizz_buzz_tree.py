9e9e0cddf434661702f7a61c568c84f5d95f97df
Matthew Holder <holdermatthew5@gmail.com>
2021-02-08 23:18:19 -0800

    assignment change

--- python/challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ python/challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,25 +1,40 @@
 class Node:
-    def __init__(self, value = None, n = []):
+    def __init__(self, value = None, left = None, right = None):
         self.value = value
-        self.n = n
+        self.left = left
+        self.right = right
     
 class KaryTree:
     def __init__(self, node=None):
-        self.tree = node
+        self.root = node
 
 def FizzBuzzTree(tree):
-    new = KaryTree(Node(tree.tree.value))
-    def traverse(node, node2):
-        if node.value % 3 == 0 and node.value % 5 == 0:
-            node2.n.append(Node('FizzBuzz'))
-        elif node.value % 3 == 0:
-            node2.n.append(Node('Fizz'))
-        elif node.value % 5 == 0:
-            node2.n.append(Node('Buzz'))
+    new = KaryTree(Node(tree.root.value))
+    def fizz_buzz(value):
+        if value % 3 == 0 and value % 5 == 0:
+            return 'FizzBuzz'
+        elif value % 3 == 0:
+            return 'Fizz'
+        elif value % 5 == 0:
+            return 'Buzz'
         else:
-            node2.n.append(Node(str(node.value)))
-        for i in range(len(node.n)):
-            print('a', node.value, 'b', node2.value)
-            traverse(node.n[2], node2.n[2])
-    traverse(tree.tree, new.tree)
+            return str(value)
+    def traverse(root1, root2):
+        if root1.left:
+            root2.left = Node(str(fizz_buzz(root1.left.value)))
+            traverse(root1.left, root2.left)
+        if root1.right:
+            root2.right = Node(str(fizz_buzz(root1.right.value)))
+            traverse(root1.right, root2.right)
+    traverse(tree.root, new.root)
     return new
+
+if __name__ == '__main__':
+    tree = KaryTree(Node(1))
+    root = tree.root
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(15)
+    print(FizzBuzzTree(tree).root.value)
