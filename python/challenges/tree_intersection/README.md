caccc2ce05ad31375b46a040ca1cb1633ca8c88d
Matthew Holder <holdermatthew5@gmail.com>
2021-03-18 23:54:50 -0700

    tree_intersection

---
+++ python/challenges/tree_intersection/README.md
@@ -0,0 +1,11 @@
+**Author:** Matthew Holder
+**Version:** 0.1
+
+[PR]()
+
+## Problem Domain:
+
+Find common values in 2 binary trees.
+
+## Description:
+
---
+++ python/challenges/tree_intersection/tree.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
